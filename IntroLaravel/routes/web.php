e16b7d4a25a4ab37983785cacd0644a13a1993a4
26-ale-20 <131723725+26-ale-20@users.noreply.github.com>
2024-09-26 23:08:24 -0600

    avances 26/09/24

--- IntroLaravel/routes/web.php
+++ IntroLaravel/routes/web.php
@@ -2,17 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// Route::get('/', function () {
+//     return view('inicio');
+// })->name('rutaInicio');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/from', function () {
+//     return view('formulario');
+// })->name('rutaFrom');
+
+// Route::get('/consultar', function () {
+//     return view('cliente');
+// })->name('rutaClientes');
+
+Route::view('/','inicio')->name('rutaInicio');
+Route::view('/from','formulario')->name('rutaFrom');
+Route::view('/consultar','cliente')->name('rutaClientes');
