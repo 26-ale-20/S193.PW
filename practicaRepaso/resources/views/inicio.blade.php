04ac552aabab3440f2c87f91a688affe6c7a6e5c
26-ale-20 <131723725+26-ale-20@users.noreply.github.com>
2024-10-17 15:09:20 -0600

    Portada 17/10/24

---
+++ practicaRepaso/resources/views/inicio.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite(['resources/js/app.js'])
+</head>
+
+
+
+<body>
+    
+ <div class="container mt-5 col-md-6">
+        <h1>Universidad Politécnica del Estado de Querétaro </h1>
+        <h2>Jimenez Hernández Verónica Alejandra</h2>
+        <h2 >Repaso 1</h2>
+        <h3>Programación web</h3>
+        <a href="/from" class="btn btn-danger"> Ir al Registro </a>
+       
+</body>
+</html>
