b1683e17e520a7e465a547b8fc6fffbb516fb2aa
26-ale-20 <131723725+26-ale-20@users.noreply.github.com>
2024-10-31 03:00:04 -0600

    validador

--- RepasoII/app/Http/Controllers/ControladorVitas.php
+++ RepasoII/app/Http/Controllers/ControladorVitas.php
@@ -17,8 +17,21 @@ class ControladorVitas extends Controller
         return view('registro');
     }
 
+
+
     public function procesarLibro(Request $peticion)
     {
+
+        $validacion = $peticion->validate([
+            'txtisbn' => 'required|numeric|digits:13', 
+            'txttitulo' => 'required|string|max:150', 
+            'txtautor' => 'required|string|max:100', 
+            'txtpaginas' => 'required|integer|min:1', 
+            'txtaño' => 'required|integer|digits:4|min:1000|max:' . date('Y'), 
+            'txteditorial' => 'required|string|max:100',
+            'txtemaildeeditorial' => 'required|email'
+        ]);
+
         $titulo = $peticion->input('txttitulo');
         session()->flash('exito', 'Se guardó el Libro: ' . $titulo);
         return to_route('rutaRegistrolibros');
--- RepasoII/resources/views/registro.blade.php
+++ RepasoII/resources/views/registro.blade.php
@@ -19,41 +19,57 @@
 
             <div class="mb-3">
                 <label for="isbn" class="form-label">ISBN:</label>
-                <input type="text" class="form-control" name="txtisbn" value="{{old('txtisbn')}}">
+                <input type="text" class="form-control" name="txtisbn" value="{{ old('txtisbn') }}">
+                <small class="text-danger fst-italic">{{$errors->first('txtisbn')}}</small>
             </div>
             <div class="mb-3">
                 <label for="isbn" class="form-label">Titulo:</label>
                 <input type="text" class="form-control" name="txttitulo" value="{{old('txttitulo')}}">
+                <small class="text-danger fst-italic">{{$errors->first('txttitulo')}}</small>
             </div>
             <div class="mb-3">
                 <label for="isbn" class="form-label">Autor:</label>
                 <input type="text" class="form-control" name="txtautor" value="{{old('txtautor')}}">
+                <small class="text-danger fst-italic">{{$errors->first('txtautor')}}</small>
             </div>
 
             <div class="mb-3">
                 <label for="isbn" class="form-label">Pagina:</label>
                 <input type="text" class="form-control" name="txtpagina" value="{{old('txtpagina')}}">
+                <small class="text-danger fst-italic">{{$errors->first('txtpagina')}}</small>
             </div>
 
             <div class="mb-3">
                 <label for="isbn" class="form-label">Año:</label>
                 <input type="text" class="form-control" name="txtaño" value="{{old('txtaño')}}">
+                <small class="text-danger fst-italic">{{$errors->first('txtaño')}}</small>
             </div>
 
             <div class="mb-3">
                 <label for="isbn" class="form-label">Editorial:</label>
                 <input type="text" class="form-control" name="txteditorial" value="{{old('txteditorial')}}">
+                <small class="text-danger fst-italic">{{$errors->first('txteditorial')}}</small>
             </div>
 
             <div class="mb-3">
                 <label for="isbn" class="form-label">Email de Editorial:</label>
                 <input type="text" class="form-control" name="txtemaildeeditorial" value="{{old('txtemaileditorial')}}">
+                <small class="text-danger fst-italic">{{$errors->first('txtemaildeeditorial')}}</small>
             </div>
 
             <button type="submit" class="btn btn-success btn-sm">Guardar</button>
         </form>
 
     </div>
+    <footer>
+    <div class="container-fluid">
+        <div class="row text-center">
+         <p>
+            © 2023 Biblioteca de Queretaro, El Pueblito Corregidora Qro. Todos los derechos reservados.
+         </p>
+         </div>
+         </div>
+</footer>
 
  </div>
 @endsection
