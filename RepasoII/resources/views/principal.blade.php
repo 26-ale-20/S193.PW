68392baa7bc32b3d35d07f61b629b29f147aa500
26-ale-20 <131723725+26-ale-20@users.noreply.github.com>
2024-10-31 01:17:02 -0600

    avance formulario

---
+++ RepasoII/resources/views/principal.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.plantilla')
+@section('contenido')
+<div class="container my-4">
+    <div class="row align-items-center">
+        <div class="col-md-8">
+            <h1 class="text-start"> SEP promete concluir entrega de libros de texto antes de que concluya septiembre</h1>
+            <p>La secretaria de Educación Pública, Leticia Ramírez Amaya, anunció que la entrega de Libros de Texto Gratuitos para el Ciclo Escolar 2024-2025 se completará antes de finalizar septiembre, asegurando que tanto escuelas públicas como particulares de educación preescolar y secundaria en todo el país recibirán los materiales necesarios. Desde el inicio del ciclo escolar, todas las escuelas primarias ya cuentan con sus ejemplares.</p>
+            <p>Ramírez Amaya también aclaró que la asignatura de Inglés tendrá libros de texto específicos para cada grado de secundaria, diferenciándolos de los materiales educativos generales. Además, se anticipa la publicación de lineamientos para el expendio de alimentos y bebidas en escuelas de nivel básico, con el objetivo de combatir problemas de salud como la obesidad y la diabetes infantil</p>
+             <p>En cuanto a la asistencia escolar, se reportó que más de 24 millones de estudiantes están presentes en las aulas, a excepción de algunas escuelas en Chalco, Chiapas y Guerrero. La secretaria destacó la autonomía docente, permitiendo a los maestros evaluar a sus alumnos más allá de los contenidos establecidos, y mencionó que se realizará la prueba PISA en algunas escuelas</p>
+             <p>Se informó sobre la próxima inauguración del Museo Vivo del Muralismo, que se completará antes de que termine el gobierno actual. También se están llevando a cabo reuniones con el equipo del próximo secretario de Educación para asegurar la continuidad de la transformación educativa. Finalmente, se discutirá la continuidad de la Comisión Nacional para la Mejora Continua de la Educación, buscando evitar duplicidades en la administración pública.</p>
+    </div>
+</div>
+<footer>
+    <div class="container-fluid">
+        <div class="row text-center">
+         <p>
+            © 2023 Biblioteca de Queretaro, El Pueblito Corregidora Qro. Todos los derechos reservados.
+         </p>
+         </div>
+         </div>
+</footer>
+@endsection
